40f05275a46ffc924d10870bcfdb3de2ece3abcc
Jasiostwor <jasiostwor1@gmial.com>
2021-10-06 16:10:11 +0200

    6.10.2021

---
+++ Aplikacje/6.10.2021/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+        <style>
+            td{
+                width: 50px;
+                height: 50px;
+                background-color: blueviolet;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        
+        for($i=0;$i<10;$i++){
+            echo '<table>';
+            echo '<tr>';
+            
+            echo '<td>';
+            echo '</td>';
+            
+            echo '<td>';
+            echo '</td>';
+            
+            echo '<td>';
+            echo '</td>';
+            
+            echo '</tr>';
+            echo '</table>';
+            } 
+        ?>
+    </body>
+</html>
